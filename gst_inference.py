36cf5b6edb4e7cd0e5a4a0812399aabb862443ea
NoVarlok <yakhtin.leonid@yandex.ru>
2022-03-11 00:20:13 +0300

    Add test version of gst inference

--- gst_inference.py
+++ gst_inference.py
@@ -33,6 +33,9 @@ from model import load_model
 
 from tps import Handler, cleaners
 # from tps import prob2bool, symbols, cleaners
+from utils.data_utils import TextMelLoader
+from scipy.io.wavfile import read
+import librosa
 
 
 def parse_input_csv(path):
@@ -55,7 +58,7 @@ def get_text(text, mask_stress, mask_phonemes, text_handler):
     return text_tensor
 
 
-def main(input_sentences, input_names, tacotron_path, hparams, use_basic_handler, charset, mask_stress, mask_phonemes, save_dir):
+def main(input_sentences, input_names, tacotron_path, audio_path, hparams, use_basic_handler, charset, mask_stress, mask_phonemes, save_dir):
     tacotron = load_model(hparams, False)
     checkpoint_dict = torch.load(tacotron_path, map_location="cpu")
     tacotron.load_state_dict(checkpoint_dict["state_dict"])
@@ -67,31 +70,39 @@ def main(input_sentences, input_names, tacotron_path, hparams, use_basic_handler
     else:
         text_handler = Handler.from_charset(charset, data_dir="data", silent=True)
 
+    text_loader = TextMelLoader(text_handler, test_csv, hparams)
+    y, sr = librosa.load(audio_path)
+    mel = torch.tensor(librosa.feature.melspectrogram(y=y, sr=sr, n_mels=hparams.n_mel_channels))
+    mel = torch.unsqueeze(text_loader.get_mel(audio_path), 0).cuda()
+    # mel = torch.unsqueeze(mel, 0).cuda()
+    print(mel.shape)
+
     with torch.no_grad():
         for input_sentence, input_name in zip(input_sentences, input_names):
             processed_sentence = get_text(input_sentence, mask_stress, mask_phonemes, text_handler)
             print('processed_sentence:', processed_sentence)
             processed_sentence = torch.unsqueeze(processed_sentence, 0)
             processed_sentence = processed_sentence.cuda()
-            mel_outputs, mel_outputs_postnet, gates, alignments = tacotron.inference(processed_sentence)
+            mel_outputs, mel_outputs_postnet, gates, alignments = tacotron.inference(processed_sentence, reference_mel=mel)
             mel_save_path = os.path.join(save_dir, f'{input_name}.pt')
             print(mel_save_path)
             torch.save(mel_outputs_postnet[0], mel_save_path)
 
 
 if __name__ == "__main__":
-    model_checkpoint_path = '/home/lyakhtin/repos/tts/results/natasha/tacotron2/tacotron2-feb-12-pretrained-from-ruslan/checkpoint_114000'
-    hparams_path = '/home/lyakhtin/repos/tts/results/natasha/tacotron2/tacotron2-feb-12-pretrained-from-ruslan/hparams.yaml'
+    model_checkpoint_path = '/home/lyakhtin/repos/tts/results/natasha/tacotron2/tacotron-gst-mar-04/checkpoint_114000'
+    hparams_path = '/home/lyakhtin/repos/tts/results/natasha/tacotron2/tacotron-gst-mar-04/hparams.yaml'
     save_dir = '/home/lyakhtin/repos/tts/results/natasha/inference_test/test_mels'
     test_csv = '/home/lyakhtin/repos/tts/datasets/natasha_dataset/tacotron_test.csv'
+    audio_path = '/home/lyakhtin/repos/tts/gst_wavs/koq1q-l5wci.wav'
     hparams = create_hparams(hparams_path)
-
     input_sentences, input_names = parse_input_csv(test_csv)
 
     main(input_sentences=input_sentences,
          input_names=input_names,
          hparams=hparams,
          tacotron_path=model_checkpoint_path,
+         audio_path=audio_path,
          use_basic_handler=True,
          charset='ru',
          mask_stress=False,
--- modules/gst.py
+++ modules/gst.py
@@ -42,12 +42,14 @@ class ReferenceEncoder(torch.nn.Module):
 
 
     def forward(self, inputs, input_lengths=None):
-        assert inputs.size(-1) % self.n_mels == 0
+        # assert inputs.size(-1) % self.n_mels == 0
+        print(inputs.size(0), inputs.size(1), inputs.size(2))
         out = inputs.view(inputs.size(0), 1, -1, self.n_mels)  # [N, 1, Ty, n_mels]
         for conv in self.convs:
             out = conv(out)
 
         out = out.transpose(1, 2)  # [N, Ty//2^K, 128, n_mels//2^K]
+        print('out:', out.shape)
         N, T = out.size(0), out.size(1)
         out = out.contiguous().view(N, T, -1)  # [N, Ty//2^K, 128*n_mels//2^K]
 
@@ -171,7 +173,10 @@ class GST(torch.nn.Module):
     def inference(self, encoder_outputs, reference_mel=None, token_idx=None):
         style_embedding = None
         if reference_mel is not None:
-            _, style_embedding = self._forward(reference_mel)
+            # _, style_embedding = self._forward(reference_mel)
+            _, style_embedding = self.forward(reference_mel)
+            print(f'encoder_outputs: {encoder_outputs.shape}')
+            print(f'style_embedding: {style_embedding.shape}')
             style_embedding = style_embedding.expand_as(encoder_outputs)
         elif token_idx is not None:
             query = self.stl.embed.new_zeros(1, 1, self.stl.query_dim)
